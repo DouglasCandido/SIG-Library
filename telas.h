dc2efc54010eb4efacbc4eec543e65afa2c169cf
Douglas Cândido <douglasmateus1@hotmail.com>
2019-08-11 17:33:15 -0300

    Header filers em construção.

---
+++ telas.h
@@ -0,0 +1,15 @@
+int menuPrincipal() {
+
+	int resp;
+
+	system("clear");
+
+	printf(" ================================= \n | | | Programa biblioteca | | | \n ================================= \n >>>>>>>  MENU PRINCIPAL  <<<<<<<< \n ================================= \n Escolha uma opção: \n []1 - LOGIN \n []2 - SAIR \n ");
+
+	printf("\nDigite: ");
+
+    scanf("%d", &resp);
+
+    return resp;
+
+}
