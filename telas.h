664cb9317e0d874686bcb7c63ac4427d1ecb7776
LucasSilva01 <53836194+LucasSilva01@users.noreply.github.com>
2019-08-12 15:23:39 -0300

    Atualização

--- telas.h
+++ telas.h
@@ -1,9 +1,10 @@
 int menuPrincipal();
 int loginAdm();
 char menuAdmin();
+int loginUser();
 char menuUser();
 int menuGerenciarEmprestimos();
-int menuLog();
+int menuLog(); 
 int menuRedefinirUser();
 
 int menuPrincipal() {
@@ -12,7 +13,7 @@ int menuPrincipal() {
 
 	system("clear");
 
-	printf(" ================================= \n | | | Programa biblioteca | | | \n ================================= \n >>>>>>>  MENU PRINCIPAL  <<<<<<<< \n ================================= \n Escolha uma opção: \n []1 - LOGIN ADM \n []2 - LOGIN USUÁRIO \n []3 - SAIR \n");
+	printf("\n ================================= \n | | | Programa Biblioteca | | | \n ================================= \n >>>>>>>  MENU PRINCIPAL  <<<<<<<< \n ================================= \n Escolha uma opção: \n []1 - LOGIN ADM \n []2 - LOGIN USUÁRIO \n []3 - Sobre \n []4 - SAIR \n");
 
 	printf("\n");
 
@@ -37,23 +38,24 @@ int loginAdm(){
 
 	system("clear");
     
-    printf("\n Login de Administrador \n");
+    printf("\n Login do Administrador \n");
 
     printf("\n Informe o nome de usuário: ");
-    scanf("%s",login1);
+    scanf("%s", login1);
 	printf(" Informe a senha: ");
-	scanf("%s",senha1);
+	scanf("%s", senha1);
 
-	a = strcmp(login,login1);
+	a = strcmp(login, login1);
 	b = strcmp(senha, senha1);
 
 	if(a == 0 && b == 0){
 		printf("\n Login realizado com sucesso.\n");
-		menuAdmin();
+		return 1;
 	}
 
 	else {
 		printf("\n Algo deu errado.\n");
+		return 0;
 	}
 	
 }
@@ -64,7 +66,7 @@ char menuAdmin() {
 
 	system("clear");
 
- 	printf(" ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU ADMINISTRADOR <<<<<<<<<<<<<<< \n ================================================= \n []Digite A - Cadastrar livro \n []Digite B - Cadastro de Pessoas \n []Digite C - Exibir informações de pessoas \n []Digite D - Excluir pessoa \n []Digite E - Exibir livros cadastrados \n []Digite F - Excluir livro cadastrado \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite I - Logs e Relatórios \n []Digite 0 - Sair \n");
+ 	printf("\n ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU ADMINISTRADOR <<<<<<<<<<<<<<< \n ================================================= \n []Digite A - Cadastrar livro \n []Digite B - Cadastro de Pessoas \n []Digite C - Exibir informações de pessoas \n []Digite D - Excluir pessoa \n []Digite E - Exibir livros cadastrados \n []Digite F - Excluir livro cadastrado \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite I - Logs e Relatórios \n []Digite S - Sair \n");
 
 	printf("\n");
 
@@ -76,6 +78,39 @@ char menuAdmin() {
 
 	return resp;
 
+}
+
+int loginUser(){
+
+    char login[51] = "teste";
+    char login1[51];
+    char senha[51] = "teste";
+    char senha1[51];
+	int a;
+	int b;
+
+	system("clear");
+    
+    printf("\n Login do Usuário \n");
+
+    printf("\n Informe o nome de usuário: ");
+    scanf("%s", login1);
+	printf(" Informe a senha: ");
+	scanf("%s", senha1);
+
+	a = strcmp(login, login1);
+	b = strcmp(senha, senha1);
+
+	if(a == 0 && b == 0){
+		printf("\n Login realizado com sucesso.\n");
+		return 1;
+	}
+
+	else {
+		printf("\n Algo deu errado.\n");
+		return 0;
+	}
+	
 }
 
 char menuUser() {
@@ -84,7 +119,7 @@ char menuUser() {
 
 	system("clear");
 
-	printf(" ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU USUÁRIO <<<<<<<<<<<<<<< \n ================================================= \n []Digite A -  \n []Digite B -  \n []Digite C -  \n []Digite D -  \n []Digite E - Exibir todos os livros do acervo \n []Digite F - Pesquisar livro no acervo \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite 0 - Sair \n");
+	printf("\n ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU USUÁRIO <<<<<<<<<<<<<<< \n ================================================= \n []Digite A -  \n []Digite B -  \n []Digite C -  \n []Digite D -  \n []Digite E - Exibir todos os livros do acervo \n []Digite F - Pesquisar livro no acervo \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite S - Sair \n");
 
 	printf("\n");
 
@@ -98,23 +133,48 @@ char menuUser() {
 
 }
 
+int menuRedefinirUser(){
+	int resp;
+
+	system("clear");
+
+	printf("\n =================================");
+	printf("\n | | | Programa Biblioteca | | |");                      
+	printf("\n =================================");
+	printf("\n >>> Alterar usuário ou senha <<<");
+	printf("\n =================================");
+	printf("\n []1 - Alterar nome de Usuário");              
+	printf("\n []2 - Alterar senha"); 
+	printf("\n []3 - Voltar ao Menu do Administrador");
+	printf("\n []4 - Sair");
+
+	printf("\n\n Escolha uma opção: ");
+	scanf(" %d", &resp);
+
+	printf("\n");
+
+    return resp;
+
+}
+
 int menuGerenciarEmprestimos(){
 	int resp;
 
 	system("clear");
 
-	printf("\n=================================");
-	printf("\n | | | Programa biblioteca | | | ");                  
-	printf("\n=================================");
-	printf("\n>>>>> GERENCIAR EMPRÉSTIMOS <<<<<");
-	printf("\n=================================");
-	printf("\n\n[]1 - Emprestar Livro");
-	printf("\n[]2 - Devolução de Livro");
-	printf("\n[]3 - Relatório");
-	printf("\n[]4 - Voltar ao Menu administrador");
+	printf("\n =================================");
+	printf("\n | | | Programa Biblioteca | | | ");                  
+	printf("\n =================================");
+	printf("\n >>>>> GERENCIAR EMPRÉSTIMOS <<<<<");
+	printf("\n =================================");
+	printf("\n []1 - Emprestar Livro");
+	printf("\n []2 - Devolução de Livro");
+	printf("\n []3 - Relatório");
+	printf("\n []4 - Voltar ao Menu do Administrador");
+	printf("\n []5 - Sair");
 
-	printf("\n\nEscolha uma opção: ");
-	scanf(" %c", &resp);
+	printf("\n\n Escolha uma opção: ");
+	scanf(" %d", &resp);
 
 	printf("\n");
 
@@ -126,17 +186,18 @@ int menuLog(){
 
 	system("clear");
 
-	printf("\n=================================");
-	printf("\n | | | Programa biblioteca | | | ");              
-	printf("\n=================================");
-	printf("\n>>>>>>>> MENU RELATÓRIO <<<<<<<<");
-	printf("\n================================="); 
-	printf("\n\n[]0 - Relatório Emprestimos");
-	printf("\n[]1 - Voltar ao Menu administrador");
-	printf("\n[]2 - Histórico de todos os empréstimos");
+	printf("\n =================================");
+	printf("\n | | | Programa Biblioteca | | | ");              
+	printf("\n =================================");
+	printf("\n >>>>>>>> MENU RELATÓRIO <<<<<<<<");
+	printf("\n ================================="); 
+	printf("\n []1 - Relatório Emprestimos");
+	printf("\n []2 - Histórico de todos os empréstimos");
+	printf("\n []3 - Voltar ao Menu do Administrador");
+	printf("\n []4 - Sair");
 
-	printf("\n\nEscolha uma opção: ");
-	scanf(" %c", &resp);
+	printf("\n\n Escolha uma opção: ");
+	scanf(" %d", &resp);
 
 	printf("\n");
 
@@ -144,25 +205,3 @@ int menuLog(){
 
  }
 
-int menuRedefinirUser(){
-	int resp;
-
-	system("clear");
-
-	printf("\n=================================");
-	printf("\n| | | Programa biblioteca | | |");                      
-	printf("\n=================================");
-	printf("\n>>> Alterar usuário ou senha <<<");
-	printf("\n=================================");
-	printf("\n\n[]1 - Alterar nome de Usuário");              
-	printf("\n[]2 - Alterar senha"); 
-	printf("\n[]3 - Sair");
-
-	printf("\n\nEscolha uma opção: ");
-	scanf(" %c", &resp);
-
-	printf("\n");
-
-    return resp;
-
-}
