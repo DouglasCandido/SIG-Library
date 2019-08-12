a6fef664b590bd3589651d0be6668c61c12003eb
Douglas Cândido <douglasmateus1@hotmail.com>
2019-08-11 21:12:50 -0300

    Add files via upload

--- esqueleto.c
+++ esqueleto.c
@@ -8,6 +8,7 @@ Professor: Flavius Gorgônio.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "telas.h"
 #include "validations.h"
 
@@ -22,20 +23,87 @@ int main(void) {
 	switch(resposta_menu_principal) {
 
 		case 1:
-			resposta_menu_admin = menuAdmin();
+			resposta_menu_admin = loginAdm();
+			switch(resposta_menu_admin) {
+
+				case 'A':
+					break;
+
+				case 'B':
+					break;
+
+				case 'C':
+					break;
+
+				case 'D':
+					break;
+
+				case 'E':
+					break;
+
+				case 'F':
+					break;
+
+				case 'G':
+					menuRedefinirUser();
+					break;
+
+				case 'H':
+					menuGerenciarEmprestimos();
+					break;
+
+				case 'I':
+					menuLog();
+					break;
+
+				case '0':
+					break;
+
+			}
 			break;
 
 		case 2:
 			resposta_menu_user = menuUser();
+			switch(resposta_menu_user) {
+				
+				case 'A':
+					break;
+
+				case 'B':
+					break;
+
+				case 'C':
+					break;
+
+				case 'D':
+					break;
+
+				case 'E':
+					break;
+
+				case 'F':
+					break;
+
+				case 'G':
+					break;
+
+				case 'H':
+					break;
+
+				case '0':
+					break;
+
+			}
 			break;
 
 		case 3:
-			system("clear");
-			printf("Você saiu do programa. Volte sempre.\n");
-			printf("\n");
 			break;
 
 	}
+
+	printf("\n");
+	printf(" Você saiu do programa. Volte sempre.\n");
+	printf("\n");
 
 	return 0;
 
--- telas.h
+++ telas.h
@@ -1,3 +1,11 @@
+int menuPrincipal();
+int loginAdm();
+char menuAdmin();
+char menuUser();
+int menuGerenciarEmprestimos();
+int menuLog();
+int menuRedefinirUser();
+
 int menuPrincipal() {
 
 	int resp;
@@ -18,13 +26,45 @@ int menuPrincipal() {
 
 }
 
-int menuAdmin() {
+int loginAdm(){
+
+    char login[51] = "admin";
+    char login1[51];
+    char senha[51] = "admin";
+    char senha1[51];
+	int a;
+	int b;
+
+	system("clear");
+    
+    printf("\n Login de Administrador \n");
+
+    printf("\n Informe o nome de usuário: ");
+    scanf("%s",login1);
+	printf(" Informe a senha: ");
+	scanf("%s",senha1);
+
+	a = strcmp(login,login1);
+	b = strcmp(senha, senha1);
+
+	if(a == 0 && b == 0){
+		printf("\n Login realizado com sucesso.\n");
+		menuAdmin();
+	}
+
+	else {
+		printf("\n Algo deu errado.\n");
+	}
+	
+}
+
+char menuAdmin() {
 
 	char resp; 
 
 	system("clear");
 
- 	printf(" ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU ADMINISTRADOR <<<<<<<<<<<<<<< \n ================================================= \n []Digite A - Cadastrar livro \n []Digite B - Cadastro de Pessoas \n []Digite C - Exibir informações de pessoas \n []Digite D - Excluir pessoa \n []Digite E - Exibir livros cadastrados \n []Digite F - Excluir livro cadastrado \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite 0 - Sair \n");
+ 	printf(" ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU ADMINISTRADOR <<<<<<<<<<<<<<< \n ================================================= \n []Digite A - Cadastrar livro \n []Digite B - Cadastro de Pessoas \n []Digite C - Exibir informações de pessoas \n []Digite D - Excluir pessoa \n []Digite E - Exibir livros cadastrados \n []Digite F - Excluir livro cadastrado \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite I - Logs e Relatórios \n []Digite 0 - Sair \n");
 
 	printf("\n");
 
@@ -38,7 +78,7 @@ int menuAdmin() {
 
 }
 
-int menuUser() {
+char menuUser() {
 
 	char resp; 
 
@@ -55,5 +95,74 @@ int menuUser() {
 	printf("\n");
 
 	return resp;
+
+}
+
+int menuGerenciarEmprestimos(){
+	int resp;
+
+	system("clear");
+
+	printf("\n=================================");
+	printf("\n | | | Programa biblioteca | | | ");                  
+	printf("\n=================================");
+	printf("\n>>>>> GERENCIAR EMPRÉSTIMOS <<<<<");
+	printf("\n=================================");
+	printf("\n\n[]1 - Emprestar Livro");
+	printf("\n[]2 - Devolução de Livro");
+	printf("\n[]3 - Relatório");
+	printf("\n[]4 - Voltar ao Menu administrador");
+
+	printf("\n\nEscolha uma opção: ");
+	scanf(" %c", &resp);
+
+	printf("\n");
+
+	return resp;
+}
+
+int menuLog(){
+	int resp;
+
+	system("clear");
+
+	printf("\n=================================");
+	printf("\n | | | Programa biblioteca | | | ");              
+	printf("\n=================================");
+	printf("\n>>>>>>>> MENU RELATÓRIO <<<<<<<<");
+	printf("\n================================="); 
+	printf("\n\n[]0 - Relatório Emprestimos");
+	printf("\n[]1 - Voltar ao Menu administrador");
+	printf("\n[]2 - Histórico de todos os empréstimos");
+
+	printf("\n\nEscolha uma opção: ");
+	scanf(" %c", &resp);
+
+	printf("\n");
+
+	return resp;
+
+ }
+
+int menuRedefinirUser(){
+	int resp;
+
+	system("clear");
+
+	printf("\n=================================");
+	printf("\n| | | Programa biblioteca | | |");                      
+	printf("\n=================================");
+	printf("\n>>> Alterar usuário ou senha <<<");
+	printf("\n=================================");
+	printf("\n\n[]1 - Alterar nome de Usuário");              
+	printf("\n[]2 - Alterar senha"); 
+	printf("\n[]3 - Sair");
+
+	printf("\n\nEscolha uma opção: ");
+	scanf(" %c", &resp);
+
+	printf("\n");
+
+    return resp;
 
 }
