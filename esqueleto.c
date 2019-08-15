040b57d08d10e689e274782a26feabe828ab1169
Douglas Cândido <douglasmateus1@hotmail.com>
2019-08-15 19:27:06 -0300

    Refinamento do código com a remoção de labels e gotos.
    O código foi modularizado com funções.

--- esqueleto.c
+++ esqueleto.c
@@ -9,187 +9,52 @@ Professor: Flavius Gorgônio.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h> 
 #include "telas.h"
 #include "validations.h"
 
 int main(void) {
 
-	int resposta_menu_principal;
+	char resposta_menu_principal;
 	char resposta_menu_admin;
 	char resposta_menu_user;
 	int resposta_menu_redefinir_user;
 	int resposta_menu_gerenciar_emprestimos;
 	int resposta_menu_log;
 
-	menu_principal: resposta_menu_principal = menuPrincipal();
+	do {
 
-	switch(resposta_menu_principal) {
-
-		case 1:
-
-			if(loginAdm() == 1) {
-
-				menu_admin: resposta_menu_admin = menuAdmin();
-
-			}
-
-			switch(resposta_menu_admin) {
-
-				case 'A':
-					goto menu_admin;
-					break;
-
-				case 'B':
-					goto menu_admin;
-					break;
-
-				case 'C':
-					goto menu_admin;
-					break;
-
-				case 'D':
-					goto menu_admin;
-					break;
-
-				case 'E':
-					goto menu_admin;
-					break;
-
-				case 'F':
-					goto menu_admin;
-					break;
-
-				case 'G':
-					resposta_menu_redefinir_user = menuRedefinirUser();
-
-					if(resposta_menu_redefinir_user == 3) {
-
-						goto menu_admin;
-
-					}
-
-					else if(resposta_menu_redefinir_user == 4){
-
-						break;
-
-					}
-
-					break;
-					
-
-				case 'H':
-					resposta_menu_gerenciar_emprestimos = menuGerenciarEmprestimos();
-
-					if(resposta_menu_gerenciar_emprestimos == 4) {
-
-						goto menu_admin;
-
-					}
-
-					else if(resposta_menu_redefinir_user == 5){
-
-						break;
-
-					}
-
-					break;
-
-				case 'I':
-					resposta_menu_log = menuLog();
-
-					if(resposta_menu_log == 3) {
-
-						goto menu_admin;
-
-					}
-
-					else if(resposta_menu_redefinir_user == 4){
-
-						break;
-
-					}
-
-					break;
-
-				case 'S':
-					break;
-
-			}
-			break;
-
-		case 2:
-
-			if(loginUser() == 1) {
-
-				menu_user: resposta_menu_user = menuUser();
-
-			}
-
-			switch(resposta_menu_user) {
-				
-				case 'A':
-					break;
-
-				case 'B':
-					break;
-
-				case 'C':
-					break;
-
-				case 'D':
-					break;
-
-				case 'E':
-					break;
-
-				case 'F':
-					break;
-
-				case 'G':
-					break;
-
-				case 'H':
-					break;
-
-				case 'S':
-					break;
-
-			}
-			break;
-
-		case 3:
-
-			printf(" A library system made with C. \n Universidade Federal do Rio Gande do Norte (UFRN). \n Students: Douglas Mateus and Lucas Silva. \n Professor: Flavius Gorgônio. \n");
-
-			int resposta;
-
-			printf("\n Deseja voltar para o Menu Principal (1) ou Sair (0)?");
-
-			scanf("%d", &resposta);
-
-			if(resposta == 1){
-
-				goto menu_principal;
-
-			}
-
-			else {
-	
+		switch(resposta_menu_principal = menuPrincipal()) {
+			
+			case 'A':
+				if(loginAdm()){
+					menuAdmin();
+				}
 				break;
+			case 'B':
+				if(loginUser()){
+					menuUser();
+				}
+				break;
+			case 'C':
+				sobre();
+				break;
+			case 'S':
+				printf("\n");
+				printf(" Você saiu do SIG-Library. Volte sempre.\n");
+				printf("\n");
+				break;
+			default:
+				printf("Você digitou uma opção errada.");
 				
-			}
-
-			break;
-
-		case 4:
-			break;
-
+			
 	}
+		
+	
+	}while(resposta_menu_principal != 'S');
 
-	printf("\n");
-	printf(" Você saiu do SIG-Library. Volte sempre.\n");
-	printf("\n");
-
+	
+			
 	return 0;
 
 }
--- telas.h
+++ telas.h
@@ -1,27 +1,37 @@
-int menuPrincipal();
-int loginAdm();
-char menuAdmin();
-int loginUser();
-char menuUser();
-int menuGerenciarEmprestimos();
-int menuLog(); 
-int menuRedefinirUser();
+char sair(void);
+char menuPrincipal(void);
+int loginAdm(void);
+void menuAdmin(void);
+int loginUser(void);
+char menuUser(void);
+char menuGerenciarEmprestimos(void);
+char menuLog(void); 
+char menuRedefinirUser(void);
+void sobre(void);
 
-int menuPrincipal() {
+char menuPrincipal() {
 
-	int resp;
+	char resp;
 
 	system("clear");
 
-	printf("\n ================================= \n | | | Programa Biblioteca | | | \n ================================= \n >>>>>>>  MENU PRINCIPAL  <<<<<<<< \n ================================= \n Escolha uma opção: \n []1 - LOGIN ADM \n []2 - LOGIN USUÁRIO \n []3 - Sobre \n []4 - SAIR \n\n(LOGIN ADM e SENHA padrão - admin)\n(LOGIN USUÁRIO e SENHA padrão - teste)\n\n");
+	printf("\n ================================= \n | | | Programa Biblioteca | | | \n ================================= \n >>>>>>>  MENU PRINCIPAL  <<<<<<<< \n ================================= \n Escolha uma opção: \n []A - LOGIN ADM \n []B - LOGIN USUÁRIO \n []C - Sobre \n []S - SAIR \n\n(LOGIN ADM e SENHA padrão - admin)\n(LOGIN USUÁRIO e SENHA padrão - teste)\n\n");
 
 	printf("\n");
 
 	printf(" Digite: ");
 
-    scanf("%d", &resp);
-
+   
+	scanf(" %c", &resp);
     printf("\n");
+    
+    
+	while(resp != 'A' && resp !=  'B' && resp != 'C' && resp != 'S') { 
+		
+		printf("\nDigite uma opção válida: ");
+		scanf(" %c", &resp);
+		
+	}
 
     return resp;
 
@@ -47,6 +57,7 @@ int loginAdm(){
 	b = strcmp(senha, senha1);
 
 	while (!(a == 0 && b == 0)){
+		
 		printf("\n\nLogin ou senha incorretos, tente novamente.");
 		printf("\nInforme o nome de usuário: ");
     	scanf("%s",login1);
@@ -58,30 +69,73 @@ int loginAdm(){
 	}
 
 	if(a == 0 && b == 0){
+		
 		printf("\n Login realizado com sucesso.\n");
 		return 1;
 	}
 	
+	return 0;
+	
 }
 
-char menuAdmin() {
+void menuAdmin() {
 
 	char resp; 
 
-	system("clear");
+	do {
+		
+		system("clear");
 
- 	printf("\n ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU ADMINISTRADOR <<<<<<<<<<<<<<< \n ================================================= \n []Digite A - Cadastrar livro \n []Digite B - Cadastro de Pessoas \n []Digite C - Exibir informações de pessoas \n []Digite D - Excluir pessoa \n []Digite E - Exibir livros cadastrados \n []Digite F - Excluir livro cadastrado \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite I - Logs e Relatórios \n []Digite S - Sair \n\n(As opções -G-,-H- e -I-, são as únicas que possuem menu.)\n\n");
+		printf("\n ================================================= \n |  |  |  |  |  Programa Biblioteca  |  |  |  |  | \n ================================================= \n >>>>>>>>>>>>>> MENU ADMINISTRADOR <<<<<<<<<<<<<<< \n ================================================= \n []Digite A - Cadastrar livro \n []Digite B - Cadastro de Pessoas \n []Digite C - Exibir informações de pessoas \n []Digite D - Excluir pessoa \n []Digite E - Exibir livros cadastrados \n []Digite F - Excluir livro cadastrado \n []Digite G - Redefinir nome de usuário ou senha \n []Digite H - Gerenciar empréstimos \n []Digite I - Logs e Relatórios \n []Digite S - Sair \n\n(As opções -G-,-H- e -I-, são as únicas que possuem menu.)\n\n");
+
+		printf("\n");
+
+		printf("Escolha uma opção: ");
+
+		scanf(" %c", &resp);
+		
+		switch(resp) {
+			
+			case 'A':
+				printf("Ainda não implementado.\n");
+				break;
+				
+			case 'B':
+				printf("Ainda não implementado.\n");
+				break;
+				
+			case 'C':
+				printf("Ainda não implementado.\n");
+				break;
+				
+			case 'D':
+				printf("Ainda não implementado.\n");
+				break;
+			
+			case 'E':
+				printf("Ainda não implementado.\n");
+				break;
+			
+			case 'F':
+				printf("Ainda não implementado.\n");
+				break;
+				
+			case 'G':
+				menuRedefinirUser();
+				break;
+				
+			case 'H':
+				menuGerenciarEmprestimos();
+				break;
+			case 'I':
+				menuLog();
+				break;
+				
+		}
+		
+	}while(resp != 'S');
 
 	printf("\n");
-
-	printf("Escolha uma opção: ");
-
-
-	scanf(" %c", &resp);
-
-	printf("\n");
-
-	return resp;
 
 }
 
@@ -117,11 +171,14 @@ int loginUser(){
 				
 
 	}
+	
 	if(a == 0 && b == 0){
+		
 		printf("\n Login realizado com sucesso.\n");
-		return 1;
+		
 	}
-
+	
+	return 1;
 	
 }
 
@@ -138,15 +195,15 @@ char menuUser() {
 	printf(" Escolha uma opção: ");
 
 	scanf(" %c", &resp);
-
 	printf("\n");
 
 	return resp;
 
 }
 
-int menuRedefinirUser(){
-	int resp;
+char menuRedefinirUser(){
+	
+	char resp;
 
 	system("clear");
 
@@ -155,13 +212,12 @@ int menuRedefinirUser(){
 	printf("\n =================================");
 	printf("\n >>> Alterar usuário ou senha <<<");
 	printf("\n =================================");
-	printf("\n []1 - Alterar nome de Usuário");              
-	printf("\n []2 - Alterar senha"); 
-	printf("\n []3 - Voltar ao Menu do Administrador");
-	printf("\n []4 - Sair");
+	printf("\n []A - Alterar nome de Usuário");              
+	printf("\n []B - Alterar senha"); 
+	printf("\n []C - Voltar ao Menu do Administrador");
 
 	printf("\n\n Escolha uma opção: ");
-	scanf(" %d", &resp);
+	scanf(" %c", &resp);
 
 	printf("\n");
 
@@ -169,8 +225,9 @@ int menuRedefinirUser(){
 
 }
 
-int menuGerenciarEmprestimos(){
-	int resp;
+char menuGerenciarEmprestimos(){
+	
+	char resp;
 
 	system("clear");
 
@@ -179,22 +236,22 @@ int menuGerenciarEmprestimos(){
 	printf("\n =================================");
 	printf("\n >>>>> GERENCIAR EMPRÉSTIMOS <<<<<");
 	printf("\n =================================");
-	printf("\n []1 - Emprestar Livro");
-	printf("\n []2 - Devolução de Livro");
-	printf("\n []3 - Relatório");
-	printf("\n []4 - Voltar ao Menu do Administrador");
-	printf("\n []5 - Sair");
+	printf("\n []A - Emprestar Livro");
+	printf("\n []B - Devolução de Livro");
+	printf("\n []C - Relatório");
+	printf("\n []D - Voltar ao Menu do Administrador");
 
 	printf("\n\n Escolha uma opção: ");
-	scanf(" %d", &resp);
+	scanf(" %c", &resp);
 
 	printf("\n");
 
 	return resp;
 }
 
-int menuLog(){
-	int resp;
+char menuLog(){
+	
+	char resp;
 
 	system("clear");
 
@@ -203,17 +260,35 @@ int menuLog(){
 	printf("\n =================================");
 	printf("\n >>>>>>>> MENU RELATÓRIO <<<<<<<<");
 	printf("\n ================================="); 
-	printf("\n []1 - Relatório Emprestimos");
-	printf("\n []2 - Histórico de todos os empréstimos");
-	printf("\n []3 - Voltar ao Menu do Administrador");
-	printf("\n []4 - Sair");
+	printf("\n []A - Relatório Emprestimos");
+	printf("\n []B - Histórico de todos os empréstimos");
+	printf("\n []C - Voltar ao Menu do Administrador");
 
 	printf("\n\n Escolha uma opção: ");
-	scanf(" %d", &resp);
+	scanf(" %c", &resp);
 
 	printf("\n");
 
 	return resp;
 
+ }
+ 
+char sair() {
+	 
+	char resp;
+	 
+	printf("\n Deseja sair do sistema? (S/N)?");
+	scanf(" %c", &resp);
+	
+	return resp;
+
+	 
+ }
+ 
+ void sobre() {
+	 
+	 printf(" A library system made with C. \n Universidade Federal do Rio Gande do Norte (UFRN). \n Students: Douglas Mateus and Lucas Silva. \n Professor: Flavius Gorgônio. \n");
+
+	 
  }
 
