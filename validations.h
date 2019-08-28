f3f747db550e5096cd66b9fff1d3542e8cbc0cef
LucasSilva01 <53836194+LucasSilva01@users.noreply.github.com>
2019-08-28 08:58:30 -0300

    Validação para caracteres

--- validations.h
+++ validations.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> 
+#include "telas.h"
 
+
+char maius(char);
+
+char maius(char a){
+    char letra;
+    letra = toupper(a);
+    return letra;
+}
