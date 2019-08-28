061bc4ce8baffae15e5e157e026bc7664bb392bf
DouglasCandido <douglasmateus1@hotmail.com>
2019-08-28 15:23:04 -0300

    Alterações na validação.

--- validations.h
+++ validations.h
@@ -1,5 +1,5 @@
 #define RE_EMAIL ".+@.+\\.[a-z]+" 
-#define RE_TEL "^\\([1-9]{2}\\) (?:[2-8]|9[1-9])[0-9]{4}\\-[0-9]{4}$"
+#define RE_TEL "[0-9]{2}-([0-9]{8}|[0-9]{9})"
 
 char maius(char r); // Essa função é responsável por converter letras minúsculas em maiúsculas para casos de validação de entradas.
 int validaEmail(char email[30]);
